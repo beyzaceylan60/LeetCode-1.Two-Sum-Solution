d6d11db6f8b86a9a4660b5990e0be06173e9d6f7
Beyza Ceylan <101511029+beyzaceylan60@users.noreply.github.com>
2024-05-11 20:28:19 +0300

    Create TwoSumLeetcode.java
    Solution of LeetCode 1. Two Sum

---
+++ TwoSumLeetcode.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] solutions = new int[2];
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    solutions[0]=i;
+                    solutions[1]=j;
+                    return solutions;
+                }
+            }
+        }
+        return solutions;
+    }
+}
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        Solution solution = new Solution();
+        int target = 9;
+        int[] nums = [2,7,11,15];
+
+        System.out.println(solution.twoSum(nums,target));
+    }
+}
